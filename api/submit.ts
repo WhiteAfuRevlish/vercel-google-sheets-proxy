b1e22e32130c64aba5770f22f2fd0cdbd9529831
WhiteAfuRevlish <90625512+WhiteAfuRevlish@users.noreply.github.com>
2025-07-12 13:09:16 +0300

    Update submit.ts

--- api/submit.ts
+++ api/submit.ts
@@ -1,4 +1,4 @@
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req, res) {
   // 1. Дозволяємо CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
